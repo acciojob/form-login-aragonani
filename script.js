3cd5a130c01d35ae958fa395ffb8921dea5fb4a7
aragonani <101066123+aragonani@users.noreply.github.com>
2025-02-25 00:47:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+	 function getFormValue(event) {
+            event.preventDefault(); // Prevent form from submitting normally
+            
+            let firstName = document.querySelector("#fname").value.trim();
+            let lastName = document.querySelector("#lname").value.trim();
+            
+            if (firstName === "" || lastName === "") {
+                alert("Please enter both First Name and Last Name.");
+            } else {
+                alert(firstName + " " + lastName);
+            }
+        }
 
 }
